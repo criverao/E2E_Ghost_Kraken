7a36b8a3240c571d6aff8365ec1e12380a7e985f
criverao <c.riverao@uniandres.edu.co>
2022-05-14 19:11:46 -0500

    Add html report to compare generated images with resembleJs

--- index.js
+++ index.js
@@ -1,32 +1,93 @@
 const compareImages = require("resemblejs/compareImages")
 const config = require("./config.json");
+const properties = require("./properties.json");
 const fs = require('fs');
+let steps = [];
+let resultInfo = {}
+const adminUrl = properties.ADMIN_URL;
+const folder1 = config.folder1;
+const folder2 = config.folder2;
+const resultDir = config.resultsFolder;
 
 async function getDiff(){
 
-    const folder1 = config.folder1;
-    const folder2 = config.folder2;
-    const resultDir = config.resultsFolder;
 
-    if (!fs.existsSync(resultDir)){
-        fs.mkdirSync(resultDir, { recursive: true });
+    let datetime = new Date().toISOString().replace(/:/g,".");
+
+    if (!fs.existsSync(`.${resultDir}`)){
+        fs.mkdirSync(`.${resultDir}`, { recursive: true });
     }
-    var step = 0;
-    fs.readdir(folder1, async (err, files) => {
-        for (const file of files) {
-            step ++;
-            const data = await compareImages(
-                await fs.readFileSync(`${folder1}/${file}`),
-                await fs.readFileSync(`${folder2}/${file}`),
-                config.options
-            );
 
-            await fs.writeFileSync(`${resultDir}/${file}`, data.getBuffer());
+    const files = fs.readdirSync(`.${folder1}`);
+    let id = 0;
+    for (const file of files) {
+        id = file.split(".")[0]
+        steps[id] = id;
+        const data = await compareImages(
+            await fs.readFileSync(`.${folder1}/${file}`),
+            await fs.readFileSync(`.${folder2}/${file}`),
+            config.options
+        );
+        resultInfo[id] = {
+            isSameDimensions: data.isSameDimensions,
+            dimensionDifference: data.dimensionDifference,
+            rawMisMatchPercentage: data.rawMisMatchPercentage,
+            misMatchPercentage: data.misMatchPercentage,
+            diffBounds: data.diffBounds,
+            analysisTime: data.analysisTime
         }
-    });
+
+        await fs.writeFileSync(`.${resultDir}/${file}`, data.getBuffer());
+
+    }
+
+    fs.writeFileSync(`.${resultDir}/report.html`, createReport(datetime, resultInfo));
+    fs.copyFileSync('./index.css', `.${resultDir}/index.css`);
 
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
 
 }
+
 getDiff();
+
+function row(step, info){
+    return `<div class=" browser" id="test ${step}">
+        <div class="btitle">
+            <h2>Paso: ${step}</h2>
+            <p>Data: ${JSON.stringify(info)}</p>
+        </div>
+        <div class="imgline">
+          <div class="imgcontainer">
+            <span class="imgname">Reference</span>
+            <img class="img2" src="${folder1}/${step}.png" id="refImage" alt="Reference">
+          </div>
+          <div class="imgcontainer">
+            <span class="imgname">Test</span>
+            <img class="img2" src="${folder2}/${step}.png" id="testImage" alt="Test">
+          </div>
+        </div>
+        <div class="imgline">
+          <div class="imgcontainer">
+            <span class="imgname">Differences</span>
+            <img class="imgfull" src="${step}.png" id="diffImage" alt="Diff">
+          </div>
+        </div>
+    </div>`
+}
+
+function createReport(datetime, resInfo){
+    return `<html>
+    <head>
+        <title>VRT Report</title>
+        <link href="index.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Report for <a href="${adminUrl}"> ${adminUrl}</a></h1>
+        <p>Executed: ${datetime}</p>
+        <div id="visualizer">
+            ${steps.map(s=>row(s, resInfo[s]))}
+        </div>
+    </body>
+</html>`
+}
