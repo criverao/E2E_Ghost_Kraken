21ccc2ec194eab695cb7750e94fb6645839083bd
criverao <c.riverao@uniandres.edu.co>
2022-05-21 10:54:03 -0500

    Add new positive scenario change settings using kraken faker

---
+++ features/change_general_settings_positive.feature
@@ -0,0 +1,49 @@
+Feature: General Settings
+
+  @user1 @web
+  Scenario: Edit general configurations for publication info
+    Given I navigate to page "<ADMIN_URL>"
+    And I wait for 1 seconds to take a screenshot
+    And I enter email "<USER>"
+    And I wait for 1 seconds to take a screenshot
+    And I enter password "<PASSWORD>"
+    And I wait for 1 seconds to take a screenshot
+    And I click Sign In
+    And I wait for 1 seconds to take a screenshot
+    # Comment -> Given login
+    When I click on Settings button
+    And I wait for 1 seconds to take a screenshot
+    And I click on General Setting button
+    And I wait for 1 seconds to take a screenshot
+
+    And I click on Expand button from "Title and Description"
+    And I wait for 1 seconds to take a screenshot
+    And I get text value in "Title"
+    And I set value "$name_1" into input "Title"
+    And I wait for 1 seconds to take a screenshot
+
+    And I get text value in "Description"
+    And I set value "$string_1" into input "Description"
+    And I wait for 1 seconds to take a screenshot
+
+    And I click on Expand button from "Publication Language"
+    And I wait for 1 seconds to take a screenshot
+    And I get text value in "Publication Language"
+    And I set value "$name_2" into input "Publication Language"
+    And I wait for 1 seconds to take a screenshot
+
+    And I click on Save button
+    And I wait for 1 seconds to take a screenshot
+    # Comment -> When finished editing general configurations for publication info
+    # Comment -> Then I check the fields were updated
+    Then I expect a "Saved" button
+
+    And I check text "$$name_1" in "Title"
+    And I check text "$$string_1" in "Description"
+    And I check text "$$name_2" in "Publication Language"
+    # Comment -> And I restore theirs initial values
+    And I restore initial value into input "Title"
+    And I restore initial value into input "Description"
+    And I restore initial value into input "Publication Language"
+    And I click on Save button
+    And I wait for 2 seconds to take a screenshot
--- features/web/step_definitions/inputs.js
+++ features/web/step_definitions/inputs.js
@@ -2,11 +2,23 @@ const {When, Then} = require("@cucumber/cucumber");
 const {expect} = require("chai");
 const fs = require("fs");
 
+const properties = require("../../../properties.json");
+let ghostVersion = properties.appVersion.replace(/\./g,"_");
+
 let featureName = featureFileInPath('./features')[0].split(".")[0];
-const dataPool = require(`../../../data_pools/${featureName}.json`);
+let dataPoolFile = `../../../data_pools/${featureName}.json`;
+
+let dataPool;
+
+if (fs.existsSync(dataPoolFile)) {
+    dataPool = require(dataPoolFile);
+} else {
+    dataPool = {};
+}
+
 let dataIndex = getRandomInt(0, dataPool.length);
 
-let initialValues = {
+let profileInitialValues = {
     'Full name': "",
     'Slug': "",
     'Email': "",
@@ -15,7 +27,18 @@ let initialValues = {
     'Bio': ""
 };
 
-let selectors = {
+let settingsInitialValues = {
+    'Title': "",
+    'Description': "",
+    'Language': ""
+}
+
+let initialValues = {
+    ...profileInitialValues,
+    ...settingsInitialValues
+}
+
+let profileSelectors = {
     'Full name': "(//input[@type='text'])[1]",
     'Slug': "(//input[@type='text'])[2]",
     'Email': "(//input[@type='email'])[1]",
@@ -24,10 +47,39 @@ let selectors = {
     'Bio': "(//textarea)[1]"
 }
 
+let settingsSelectors = {
+    'Title': "(//input[@type='text'])[1]",
+    'Description': "(//input[@type='text'])[2]",
+    'Publication Language': "(//input[@type='text'])[3]"
+}
+
+let selectors = {
+    ...profileSelectors,
+    ...settingsSelectors
+}
+
+const expandButtonSelectors  = {
+    "v3_42": {
+        "Title and Description": "(//button[@type='button'])[2]",
+        "Publication Language": "(//button[@type='button'])[4]",
+        "Site timezone": "(//button[@type='button'])[3]"
+    },
+    "v4_41_3": {
+        "Title and Description": "(//button[@type='button'])[3]",
+        "Publication Language": "(//button[@type='button'])[5]",
+        "Site timezone": "(//button[@type='button'])[4]"
+    }
+};
+
 When('I click on input {string}', async function (inputName) {
     let selector = selectors[inputName]
     let element = await this.driver.$(selector);
     return element.click();
+});
+
+When('I click on Expand button from {string}', async function (button) {
+    let element = await this.driver.$(expandButtonSelectors[ghostVersion][button]);
+    return await element.click();
 });
 
 When('I get text value in {string}', async function (inputName) {
@@ -43,10 +95,22 @@ When('I set value from data pool into input {string}', async function (inputName
     return element.setValue(dataPool[dataIndex][inputName]);
 });
 
+When('I set value {kraken-string} into input {string}', async function (text, inputName) {
+    let selector = selectors[inputName];
+    let element = await this.driver.$(selector);
+    return await element.setValue(text);
+});
+
 Then('I check text updated in {string}', async function (inputName) {
     let selector = selectors[inputName];
     let elementValue = await this.driver.$(selector).getValue();
     expect(elementValue).to.equal(dataPool[dataIndex][inputName]);
+});
+
+Then('I check text {kraken-string} in {string}', async function (text, inputName) {
+    let selector = selectors[inputName];
+    let elementValue = await this.driver.$(selector).getValue();
+    expect(elementValue).to.equal(text);
 });
 
 Then('I expect text was not updated in {string}', async function (inputName) {
@@ -66,6 +130,17 @@ Then('I restore initial value into input {string}', async function (inputName) {
     return element.setValue(initialValues[inputName]);
 });
 
+When('I click on select Timezone', async function () {
+    let element = await this.driver.$("select[name='general[timezone]']");
+    return await element.click();
+});
+
+When('I select the Timezone option {string}', async function (timezone) {
+    let element = await this.driver.$("option[value='" + timezone + "']");
+    return await element.click();
+});
+
+// General functions
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
